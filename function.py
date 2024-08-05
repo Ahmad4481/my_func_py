f552695f3e1327c380e434392da6b73ae8f78d36
FaisalFahad2 <fhd20677@gmail.com>
2024-08-05 21:59:44 +0300

    i not have discription

--- function.py
+++ function.py
@@ -36,3 +36,81 @@ def solution(s):
     return re.findall(".{2}",s+"_")   
 
 print(solution('abc'),solution('abcdef'))
+
+def count (string,a) -> int:
+  string=str(string)
+  if type(string)==list:
+    def to_list(list):
+      word = ''
+      for item in list:
+        word+= str(item)
+      word= string
+      return word
+    string=(to_list(string))
+  a=str(a)
+  w=0
+  for i in string:
+    if i == a:
+      w+=1
+  return w
+# print(count('elzero','e'))
+
+def repeat(string:str,re:int,space=' ',end_space:bool=False):
+  integar= string
+  string=str(string)
+  word = ''
+  for i in range(re):
+    if end_space== False:
+      word+= string
+      if re-1 > i:
+        word += space
+    else:
+      word+= string+space
+  return word
+# print (repeat(8888,6,'#',True))
+
+def zfill(st,width:int,fill:str='0') -> str:
+  st=str(st)
+  len(st)
+  while True:
+    if len(st)==width:
+      return st
+    else:
+      st=fill+st
+# print(zfill('999',4,'#'))
+
+def reverse(st:str):
+  word = ''
+  for i in st:
+    word= i+word
+  return word
+# print(reverse('orezle'))
+
+def minmal(lst,minOrMax='minmal'):
+  num = lst[0]
+  for i in lst:
+    if minOrMax=='minmal':
+      if i<num:
+        num=i
+    elif minOrMax=='max' or 'maxmal':
+      if i>num:
+        num=i
+  return num
+# print(minmal([-220,392,933,444]))
+
+def idn(st:str,align='all',og=' '):
+  if align=='right' or align=='all':
+    for a in st:
+      if a!=og:
+        break
+      st=st[0:len(st)-1]
+  if align=='left' or align=='all':
+    st=reverse(st)
+    for i in st:
+      if st[-1]!=og:
+        st=reverse(st)
+        break
+      st=st[0:len(st)-1]
+  return st
+print(idn('elzero','all',' ')) # elzero### or elzero or ###elzero
+
